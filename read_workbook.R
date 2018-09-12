22bfda68b5f87b57000107ccdbad599185ad1024
John Clark <SuperCroup@users.noreply.github.com>
2018-09-12 11:51:17 +1000

    Create read_workbook.R

---
+++ read_workbook.R
@@ -0,0 +1,19 @@
+read_workbook <- function(workbook) {
+    
+    # A function for reading and saving all the sheets in an Excel workbook
+    #
+    # Arguments:
+    # workbook  -   character string that is the path to the Excel workbook to read (.xlx or .xlsx)
+    
+    
+    require(readxl) # readxl package, part of the Tidyverse
+    
+    worksheets <- excel_sheets(workbook)
+    
+    for (worksheet in seq_along(worksheets)) { 
+        assign(worksheets[worksheet], 
+               value = read_excel(workbook, worksheet), 
+               pos = 1) # puts the result in the Global environment
+        }
+    
+}
