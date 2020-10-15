c743325be6d47407b9dc1e7a6bea722e410a2304
John Avocado <SuperCroup@users.noreply.github.com>
2020-10-15 12:22:42 +1100

    Update load_packages.R

--- load_packages.R
+++ load_packages.R
@@ -1,5 +1,5 @@
 packages_to_load <- list("tidyverse", "scales", "knitr", "lubridate", 
-						 "RColorBrewer", "readxl", "kableExtra", "glue")
+						 "RColorBrewer", "readxl", "kableExtra", "glue", "janitor")
 
 lapply(packages_to_load, library, character.only = TRUE)
 
